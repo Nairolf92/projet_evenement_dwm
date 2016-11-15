f29a5cfc5362b50dab948f0930107afa04119b0e
Florian Kelnerowski <f.kelnerowski@gmail.com>
2016-11-15 14:02:45 +0100

    Login + table users v1

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -4,10 +4,13 @@ namespace AdminBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Session\Session;
 use AdminBundle\Entity\User;
+use AdminBundle\Entity\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -43,19 +46,41 @@ class DefaultController extends Controller
  	/**
      * @Route("/admin/login", name="login")
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
+    	// PARTIE SESSION
     	$session = new Session();
-    	//connexion
-    	if($session->get('allowed') == true)
+    	
+    	/*if($session->get('allowed') == true)
     	{
 			return $this->redirectToRoute('admin');
-    	}
-    	$session->set('allowed', true);
+    	}*/
+    	$session->set('allowed', false);
         $allowed = $session->get('allowed');
+
+        // PARTIE FORMULAIRE
+    	$admin = new Admin();
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $admin);
+
+        // On ajoute les champs de l'entité que l'on veut à notre formulaire
+        $formBuilder
+            ->add('login',     TextType::class)
+            ->add('password',     TextType::class)
+            ->add('save',      SubmitType::class)
+        ;
+
+        $form = $formBuilder->getForm();
+
+        if ($request->isMethod('POST')) {
+          	$form->handleRequest($request);
+          	if ($form->isValid()) {
+
+          	}
+        }
 
     	return $this->render('AdminBundle:Default:login.html.twig', array(
    			'allowed' => $allowed,
+   			'form' => $form->createView()
 		));
     }
 }
